c573fd26a2274f4dcbd01517a9c2881518bc2c62
tomas303 <tomashorak@post.cz>
2019-01-17 07:01:51 +0100

    add variant of measure durataion of function withot parameters

--- tutils.fsx
+++ tutils.fsx
@@ -6,13 +6,13 @@ module Log =
 
     let write x =
         printfn "%A" x
-        
+
     let writeVal n v =
         printfn "%s=%A" n v
 
 module Timer =
 
-    let measure f x = 
+    let measure f x =
         let proc = Process.GetCurrentProcess()
         let cpu_time_stamp = proc.TotalProcessorTime
         let timer = new Stopwatch()
@@ -24,7 +24,19 @@ module Timer =
             printfn "CPU time = %dms" (int64 cpu_time)
             printfn "Absolute time = %dms" timer.ElapsedMilliseconds
 
-    let repeatit n f x = 
+    let measure2 f =
+        let proc = Process.GetCurrentProcess()
+        let cpu_time_stamp = proc.TotalProcessorTime
+        let timer = new Stopwatch()
+        timer.Start()
+        try
+            f
+        finally
+            let cpu_time = (proc.TotalProcessorTime-cpu_time_stamp).TotalMilliseconds
+            printfn "CPU time = %dms" (int64 cpu_time)
+            printfn "Absolute time = %dms" timer.ElapsedMilliseconds
+
+    let repeatit n f x =
         for i in 1 .. n-1 do
             f x |> ignore
         f x
