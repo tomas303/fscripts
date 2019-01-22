39a4495ec915a8b93466a343eccfa819fdf6bdab
tomas303 <tomashorak@post.cz>
2019-01-22 07:42:23 +0100

    add partial active patterns for regex

--- cheatRegEx.fsx
+++ cheatRegEx.fsx
@@ -13,6 +13,33 @@ let main argv =
     let m = m.NextMatch()
     Log.write [ for g in m.Groups -> g.Value ]
 
+    let (|RegexContains|_|) pattern input =
+        let matches = Regex.Matches(input, pattern)
+        if matches.Count > 0 then Some [ for m in matches -> m.Value ]
+        else None
+
+    match "whale" with
+    | RegexContains "\d+" value -> Log.write "cislo"
+    | RegexContains "\w+" value -> Log.write "slovo"
+    | _ -> Log.write "neco jineho"
+
+
+
+    let (|RegexMatch|_|) pattern input =
+        let matches = Regex.Matches(input, pattern)
+        Log.writeVal "pattern" pattern
+        Log.writeVal "mc" matches.Count
+        if matches.Count > 0 then Some [ for m in matches -> m ]
+        else None
+
+    match "whale|shark" with
+    | RegexMatch "\d+" rm -> Log.write rm
+    | RegexMatch "\w+" rm -> Log.write rm
+    | _ -> Log.write "neco jineho"
+
+
+
+
     0
 
 
