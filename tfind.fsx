4327c477a99dc06441a43b41dc018b2a0f6d1e17
tomas303 <tomashorak@post.cz>
2019-01-25 18:50:29 +0100

    extend parameters to command line style search, replace, help and rename fileTest to FileFind

--- tfind.fsx
+++ tfind.fsx
@@ -10,10 +10,12 @@ type OptCommand =
     | Help
 type OptDir = OptDir of string
 type OptRegex = OptRegex of string
+type OptReplacement = OptReplacement of string
 type CmdLineOptions = {
     command: OptCommand;
     dir: OptDir;
     regex: OptRegex;
+    replacement: OptReplacement;
     }
 
 
@@ -27,6 +29,12 @@ type FileMatches = {
 }
 
 let main args =
+
+    let regexEmpty(OptRegex regex) =
+        regex.Length = 0
+
+    let replacementEmpty(OptReplacement replacement) =
+        replacement.Length = 0
 
     let rec parseArgsRec args opts =
         match args with
@@ -47,6 +55,18 @@ let main args =
         | ("/r"|"-r"|"--regex")::x::t ->
             let newopts = { opts with regex = OptRegex(x)}
             parseArgsRec t newopts
+        | ("/p"|"-p"|"--replacement")::x::t ->
+            let newopts = { opts with replacement = OptReplacement(x)}
+            parseArgsRec t newopts
+        | x::t when opts.command = Search && regexEmpty opts.regex ->
+            let newopts = { opts with regex = OptRegex(x)}
+            parseArgsRec t newopts
+        | x::t when opts.command = Replace && regexEmpty opts.regex ->
+            let newopts = { opts with regex = OptRegex(x)}
+            parseArgsRec t newopts
+        | x::t when opts.command = Replace && not(regexEmpty opts.regex) && replacementEmpty opts.replacement ->
+            let newopts = { opts with replacement = OptReplacement(x)}
+            parseArgsRec t newopts
         | x::t ->
             eprintfn "Option '%s' is unrecognized" x
             parseArgsRec t opts
@@ -54,7 +74,8 @@ let main args =
     let defaultOptions = {
         command = Search;
         dir = OptDir(Directory.GetCurrentDirectory());
-        regex = OptRegex(".*");
+        regex = OptRegex("");
+        replacement = OptReplacement("");
         }
 
     let parseArgs args =
@@ -62,7 +83,7 @@ let main args =
 
     let options = parseArgs (Array.toList args)
 
-    let fileTest regex file =
+    let fileFind regex file =
         let m = RX.find (File.ReadAllText(file)) regex
         match m with
         | [] ->
@@ -73,7 +94,7 @@ let main args =
     let search =
         let (OptRegex regex) = options.regex
         let (OptDir dir) = options.dir
-        let result = Seq.fold (fun result file -> (fileTest regex file)::result) [] (FI.files dir)
+        let result = Seq.fold (fun result file -> (fileFind regex file)::result) [] (FI.files dir)
         List.rev result
 
     let printResult fileMatch =
@@ -89,9 +110,10 @@ let main args =
             printfn "0 in %s" fileMatch.fileName
             printfn "\n"
 
-    let hits = search
+    //let hits = search
     //Log.write hits
-    hits |> List.map printResult |> ignore
+    //hits |> List.map printResult |> ignore
+    Log.write options
     0
 
 #if INTERACTIVE
